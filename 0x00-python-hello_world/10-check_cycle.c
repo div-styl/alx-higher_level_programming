907ced476917cdc933630963cdba4a9dcf4fb930
div-styl <gouhmada@gmail.com>
2023-06-05 19:07:59 +0100

    betty really again

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,21 +3,20 @@
  * check_cycle - func check if the signl linked list has a check_cycle
  * @list: list to be checked
  * Return: 0 or 1
- */ 
+ */
 int check_cycle(listint_t *list)
 {
-  listint_t *slow = list;
-  listint_t *thunder = list;
+	listint_t *slow = list;
+	listint_t *thunder = list;
 
-  if (list == NULL || list->next == NULL)
-    return (0);
-  for (;thunder != NULL && thunder->next != NULL;)
-  {
-    slow = slow->next;
-    thunder = thunder->next->next;
-    if (slow == thunder)
-      return (1);
-  }
-  return (0);
+	if (list == NULL || list->next == NULL)
+		return (0);
+	for (; thunder != NULL && thunder->next != NULL;)
+	{
+		slow = slow->next;
+		thunder = thunder->next->next;
+		if (slow == thunder)
+			return (1);
+	}
+	return (0);
 }
-
