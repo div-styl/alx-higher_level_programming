082ed1a7679866de6b968a07fbad3ba6cbd03e6d
div-styl <gouhmada@gmail.com>
2023-06-06 22:24:17 +0100

    lemme see again

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     newstring = ''
-    for i in str:
+    i = 0
+    for _ in str:
         if i != n:
             newstring += str[i]
         i += 1
