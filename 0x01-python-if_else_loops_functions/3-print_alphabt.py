1424e7d3934f4cc56b16dda1e0f053351f14dd80
div-styl <gouhmada@gmail.com>
2023-06-06 18:32:49 +0100

    naaaaaaaah

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-r = ''
 for i in range(97, 122 + 1):
     if i == 101 or i == 113:
         continue
-    r += chr(i)
-#print('{}'.format(r), end='')
-print(r.format(*r), end='')
+    print('{}'.format(chr(i)), end='')
