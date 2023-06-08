3653ff6c4ff489b823dfad6a39bc55a5c9d0bfa3
div-styl <gouhmada@gmail.com>
2023-06-08 12:52:34 +0100

    oh shit here we go again

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+if __name__ == '__main__':
+    """
+    this program do the job of addition of one number
+    and more. the numbers are taken in method of argv
+    """
+import sys
+argvlen = len(sys.argv) - 1
+if (argvlen == 0):
+    print('{}'.format(argvlen))
+elif argvlen == 1:
+    print('{}'.format(sys.argv[1]))
+    exit()
+elif argvlen >= 2:
+    sum = 0
+    for i in range(1, argvlen + 1):
+        sum += int(sys.argv[i])
+print(sum)
