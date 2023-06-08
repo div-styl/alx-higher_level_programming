e2470a5befe8f1bbd7c0ab7e660bbbb2762caf08
div-styl <gouhmada@gmail.com>
2023-06-08 19:30:00 +0100

    lemme see again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    """count the number of arguments"""
-    import sys
-
-    argucnt = len(sys.argv) - 1
-    # our condi that will demonstaret our program
-    if argucnt == 0:
-        print("0 arguments.")
-    elif argucnt == 1:
-        print('1 argument:')
-    else:
-        print('{} arguments:'.format(argucnt))
-    for i in range(argucnt):
-        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+    """count the number of arguments that are given by user"""
+import sys
+len_arg = len(sys.argv) - 1
+if len_arg == 0:
+    print('{} argument.'.format(len_arg))
+elif len_arg == 1:
+    print('1 argument: ')
+else:
+    print('{} arguments: '.format(len_arg))
+for i in range(1, len_arg + 1):
+    print('{}: {}'.format(i, sys.argv[i]))
