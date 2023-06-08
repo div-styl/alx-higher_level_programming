9b7772fe9b01b65f63bd02cfc292cc4fd2987ee3
div-styl <gouhmada@gmail.com>
2023-06-08 11:19:39 +0100

    task 1 small calcu

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ =="__main__":
+    """small calculatore that calculator assigned
+    values
+    """
+from calculator_1 import *
+numo = 10
+nums = 5
+print('{} + {} = {}'.format(numo, nums, add(numo, nums)))
+print('{} + {} = {}'.format(numo, nums, sub(numo, nums)))
+print('{} + {} = {}'.format(numo, nums, mul(numo, nums)))
+print('{} + {} = {}'.format(numo, nums, div(numo, nums)))
