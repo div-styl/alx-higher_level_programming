dcc39dedfaa77b1fecba09e34cdc9413c3df67a5
div-styl <gouhmada@gmail.com>
2023-06-08 19:37:51 +0100

    really

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    """count the number of arguments that are given by user"""
+    """count the number of arguments if there is one"""
 import sys
-len_arg = len(sys.argv) - 1
-if len_arg == 0:
-    print('0 arguments.')
-elif len_arg == 1:
+"""
+get the len of the arguments 1st and then set condi
+the -1 can be consider as an overwrite of arguments counter
+the name of program consider as 0 and 1st argu is 1
+therefor the -1 ghost the name of program and convert
+the 1st arguments from value one to zero
+"""
+argucnt = len(sys.argv) - 1
+# our condi that will demonstaret our program
+if argucnt == 0:
+    print('{} arguments.'.format(argucnt))
+elif argucnt == 1:
     print('1 argument: ')
 else:
-    print('{} arguments: '.format(len_arg))
-for i in range(len_arg):
-    print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+    print('{} arguments:'.format(argucnt))
+for i in range(1, argucnt + 1):
+    print('{}: {}'.format(i, sys.argv[i]))
