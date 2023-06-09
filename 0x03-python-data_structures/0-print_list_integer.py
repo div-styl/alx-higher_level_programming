f383249dfa9b36ee808b6a26e8b890ea03bfdd3c
div-styl <gouhmada@gmail.com>
2023-06-09 22:54:57 +0100

    files are add

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,6 +5,3 @@ if __name__ == "__main__":
         for numbers in my_list:
             if isinstance(numbers, int):
                 print('{}'.format(numbers))
-
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
