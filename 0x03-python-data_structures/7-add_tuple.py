a5cddda08b06d7d2b028c96c101aadcb5dff94e9
div-styl <gouhmada@gmail.com>
2023-06-10 19:24:54 +0100

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    rslt_0 = 0
+    rslt_1 = 0
+    if len(tuple_a) > 0:
+        rslt_0 += tuple_a[0]
+    if len(tuple_a) > 1:
+        rslt_1 += tuple_a[1]
+    if len(tuple_b) > 0:
+        rslt_0 += tuple_b[0]
+    if len(tuple_b) > 1:
+        rslt_1 += tuple_b[1]
+    return (rslt_0, rslt_1)
+
