9cad8b4dd25330960b5885c9d301084880a5f143
div-styl <gouhmada@gmail.com>
2023-06-10 21:51:09 +0100

    update to task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for elements in row:
+            print('{:d}'.format(elements), end=' '
+                  if elements != row[-1] else "")
+        print()
