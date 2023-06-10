f7c17166c4fa13e2813f8803efa42103aa893a5e
div-styl <gouhmada@gmail.com>
2023-06-10 13:19:59 +0100

    task 3 and 4 removed the the if name

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """
-    a function that prints all integer
-    of a list, in reverse order.
-    """
-    def print_reversed_list_integer(my_list=[]):
-        for i in range(len(my_list)):
-            print("{}".format(my_list[i]))
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
