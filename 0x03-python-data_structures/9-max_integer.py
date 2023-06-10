3d3fdae90b1514ae2d271b6d7d744e4d99b31948
div-styl <gouhmada@gmail.com>
2023-06-11 00:32:09 +0100

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    """return the largest value with in the list"""
+    largest_value = None
+    len_list = len(my_list)
+    if len_list < 0:
+        return (None)
+    for i in my_list:
+        if largest_value is None or largest_value < i:
+            largest_value = i
+    return (largest_value)
