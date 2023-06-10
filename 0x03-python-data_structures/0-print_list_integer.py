56b1bfcd5c440e0572260b23e8e2604a03738e8c
div-styl <gouhmada@gmail.com>
2023-06-10 22:06:55 +0100

    task zero

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """print element within a list"""
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
