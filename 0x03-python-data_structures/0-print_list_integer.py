552dad435a2a0150dbf38d85c894f1213be9ae41
div-styl <gouhmada@gmail.com>
2023-06-10 19:42:48 +0100

    task 0 common

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """print elements within a list with new line"""
-    for i in range(my_list):
-        print('{}'.format(my_list[i]))
+    """print element within a list"""
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
