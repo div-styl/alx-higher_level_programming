4b88a0d6eb482c86974d29960725b1c9713cd00c
div-styl <gouhmada@gmail.com>
2023-06-11 15:48:58 +0100

    task 13 c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+/**
+* reverselist - reverse the list
+*@head: the head of the list
+* Return: void
+*/
+void reverselist(listint_t **head)
+{
+	listint_t *past = NULL, *futur = NULL, *presence = *head;
+
+	while (presence != NULL)
+	{
+		futur = presence->next;
+		presence->next = past;
+		past = presence;
+		presence = futur;
+	}
+	*head = past;
+}
+/**
+* is_palindrome - function check if the list is palindrome
+*@head: the head of the list
+*Return: either 0 or 1
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head, *fast = *head;
+	listint_t *prev = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1); /* check if the list is empty */
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+	prev->next = NULL;
+	reverselist(&slow);
+	listint_t *ptr1 = *head;
+	listint_t *ptr2 = slow;
+
+	while (ptr1 != NULL && ptr2 != NULL)
+	{
+		if (ptr1->n != ptr2->n)
+		{
+			reverselist(&slow);
+			prev->next = slow;
+			return (0);
+		}
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next;
+	}
+	reverselist(&slow);
+	prev->next = slow;
+	return (1);
+}
+
