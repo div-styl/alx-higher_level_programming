b325d03f95a78a8450f1b92f2bff8f9fc87d9780
div-styl <gouhmada@gmail.com>
2023-06-11 10:17:20 +0100

    new upload

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """print element within a list"""
+    if my_list is None:
+        return (my_list)
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
