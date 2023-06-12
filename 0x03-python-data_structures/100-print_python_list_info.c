9a1d662afb277e309bbea57fc8f24056cbbbdf4a
div-styl <gouhmada@gmail.com>
2023-06-13 00:47:38 +0100

    last task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,20 @@
+#include <Python.h>
+#include <stdio.h>
 
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t s, meme, d;
+    PyObject *i;
+
+    size = PyList_Size(p);
+    alloc = ((PyListObject *)p)->allocated;
+
+    printf("[*] Size of the Python List = %ld\n", s);
+    printf("[*] Allocated = %ld\n", meme);
+
+    for (d = 0; d < s; d++)
+    {
+        i = PyList_GetItem(p, d);
+        printf("Element %ld: %s\n", d, Py_TYPE(i)->tp_name);
+    }
+}
