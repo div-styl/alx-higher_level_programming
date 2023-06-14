e9df9ac9861da1bb1248772185c72831238582a6
div-styl <gouhmada@gmail.com>
2023-06-14 12:44:42 +0100

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    sqr_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        sqr_matrix[i] = list(map(lambda elements: elements ** 2, matrix[i]))
+    return (sqr_matrix)
