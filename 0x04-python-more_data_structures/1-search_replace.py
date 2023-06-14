b0a16546e7a29f8c6d19f0bfc7052d151d2f098f
div-styl <gouhmada@gmail.com>
2023-06-14 13:10:02 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for idx, New_value in enumerate(new_list):
+        if New_value != search:
+            return (my_list)
+        if New_value == search:
+            new_list[idx] = replace
+    return (new_list)
