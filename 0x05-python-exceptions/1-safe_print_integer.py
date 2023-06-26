dafdf984060ed6cba59e66e3971d302a0eddff18
div-styl <gouhmada@gmail.com>
2023-06-26 21:58:11 +0100

    task 3

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    """
+    argu:
+        value: the interger to be printed
+    Return:
+        priny the integer if it isn't print error
+    """
     try:
         print('{:d}'.format(value))
         return (True)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def safe_print_division(a, b): pass
+def safe_print_division(a, b):
+    """
+    function that do the arthamtic division
+    Argu:
+        a and b: are numbers
+    Return:
+        return the total sum if the division possible
+        or None if not
+    """
+    try:
+        sum = a / b
+    except (TypeError, ZeroDivisionError):
+        sum = None
+    finally:
+        print('Inside result: {}'.format(sum))
+    return (sum)
