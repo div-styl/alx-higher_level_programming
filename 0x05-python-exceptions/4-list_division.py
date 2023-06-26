efc72c3b438d3520d74acfb09a9daed35802ced3
div-styl <gouhmada@gmail.com>
2023-06-26 23:21:45 +0100

    task 6 and 4 updated

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             sum = 0
         except (IndexError):
             print('out of range')
+            div = 0
         finally:
             new_list.append(sum)
     return (new_list)
