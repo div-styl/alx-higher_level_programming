3e896580b2de37f87a7dc0db4050f520c0653d7c
div-styl <gouhmada@gmail.com>
2023-06-27 12:20:40 +0100

    101 updated

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         rst = fct(*args)
         return (rst)
-    except (IndexError):
+    except (IndexError, ZeroDivisionError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
