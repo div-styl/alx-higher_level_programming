b4fbf542d24ebcdbd93efe3fe90f065963f48423
div-styl <gouhmada@gmail.com>
2023-06-27 01:12:01 +0100

    task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+import sys
 
+
+def safe_function(fct, *args):
+    try:
+        rst = fct(*args)
+        return (rst)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
