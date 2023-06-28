f4efd74dc05d2afb509e6b4da52e9fe23a47a8b0
div-styl <gouhmada@gmail.com>
2023-06-28 13:28:46 +0100

    ok I forgot the main to test in file so ofc there is some errors

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -79,17 +79,3 @@ class SinglyLinkedList:
             tmp = tmp.next_node
         return ('\n'.join(value))
 
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
