be54611738147983841266f6bdf7ade077c7896b
div-styl <gouhmada@gmail.com>
2023-07-06 01:43:52 +0100

    task 32

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -3,4 +3,4 @@
 def magic_string():
     """funct print BestSchool in certain pattren"""
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool" + "BestSchool" * (magic_string.count - 1)
+    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
