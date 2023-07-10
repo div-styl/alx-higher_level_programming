2ffb3b8c8f7c8817b0b97e62ca850bf0148902ce
div-styl <gouhmada@gmail.com>
2023-07-10 21:06:22 +0100

    update test file for 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,8 +6,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Represents a rectangle class"""
     def __init__(self, width, height):
-        """
-        
+        """ init values
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the Rectangle
         """
         super().__init__()
     
