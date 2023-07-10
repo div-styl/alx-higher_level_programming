7dab60a3462bb77f69b69da81a1e1d6a487b1829
div-styl <gouhmada@gmail.com>
2023-07-10 23:08:42 +0100

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,46 @@
 #!/usr/bin/python3
+"""define a class"""
+
+
+class BaseGeometry:
+    """representation of a class"""
+    def area(self):
+        """function raise error"""
+        raise Exception("area () is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate that a value is an integer greater than 0.
+        Args:
+            name (str): The name of the value being validated.
+            value: The value to be validated.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not greater than 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle class"""
+    def __init__(self, width, height):
+        """ init values
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ return the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """print the rectangle"""
+        return "[Rectangle] {:d}/{:d}.format(self.__width, self.__height)"
