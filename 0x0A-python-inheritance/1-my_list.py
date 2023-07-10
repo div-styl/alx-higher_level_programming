59b3232bf24487951b33eb77eabca541597b12a7
div-styl <gouhmada@gmail.com>
2023-07-10 18:38:30 +0100

    task 1 sorting a list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""define a class inherited from lits"""
+
+
+class MyList(list):
+    """define a class"""
+    def print_sorted(self):
+        """function to print the list"""
+        print(sorted(self))
