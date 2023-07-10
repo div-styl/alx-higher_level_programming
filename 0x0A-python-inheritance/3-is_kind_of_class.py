0f5010c962e9ff14d27a800649b5d4f3c9e7404f
div-styl <gouhmada@gmail.com>
2023-07-10 19:37:51 +0100

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""function returning true obj is an instance ir false if not"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return true or fales"""
+    return isinstance(obj, a_class)
