52667ada7fb16f57b244022f26ebe357e0c8b983
div-styl <gouhmada@gmail.com>
2023-07-10 20:51:06 +0100

    test file?

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
 """define a class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """representation of a class"""
-    def area(self):
-        """function raise error"""
-        raise Exception("area() is not implemented")
- 
-    def integer_validator(self, name, value):
+class Rectangle(BaseGeometry):
+    """Represents a rectangle class"""
+    def __init__(self, width, height):
         """
-        Validate that a value is an integer greater than 0.
         
-        Args:
-            name (str): The name of the value being validated.
-            value: The value to be validated.
-        
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is not greater than 0.
         """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        super().__init__()
+    
