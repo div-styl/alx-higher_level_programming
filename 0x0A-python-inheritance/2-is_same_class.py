c9a1d469dd1ebaf150449b7f513ec68d5023a080
div-styl <gouhmada@gmail.com>
2023-07-10 19:33:10 +0100

    task 2 check if the obj is instance of specific class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""function returning true or false if the obj is instance"""
+
+
+def is_same_class(obj, a_class):
+    """return true if obj is instance of spcific class"""
+    return (type(obj) == a_class)
