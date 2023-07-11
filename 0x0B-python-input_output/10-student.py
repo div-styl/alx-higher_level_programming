e844b482f934fb0f2d3956ba21784cd9083dad1c
div-styl <gouhmada@gmail.com>
2023-07-11 12:47:20 +0100

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""define a class student"""
+
+
+class Student:
+    """represent the class student"""
+    def __init__(self, first_name, last_name, age):
+        """init values in the argumen func"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        func that retrieves a dictionary
+        representation of a Student
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            j_list = {}
+            for atr in range(len(attrs)):
+                for tar in obj:
+                    if attrs[atr] == tar:
+                        j_list[tar] = obj[atr]
+            return j_list
+        return obj
