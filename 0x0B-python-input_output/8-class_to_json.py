cca612ba14f3aeb88b61ddff35acc735bad5c824
div-styl <gouhmada@gmail.com>
2023-07-11 12:32:42 +0100

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""define a class class_to_json"""
+
+
+def class_to_json(obj):
+    """represent a class to json"""
+    result = {}
+    if hasattr(obj, "__dict__"):
+        result = obj.__dict__.copy()
+    return result
