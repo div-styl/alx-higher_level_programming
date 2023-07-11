70130ef06b5f86c8110af90c0d06f2f6886b29ca
div-styl <gouhmada@gmail.com>
2023-07-11 11:09:42 +0100

    write overwrite and create func

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""define func write_file"""
+
+
+def write_file(filename="", text=""):
+    """return a number of characters written in text"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
