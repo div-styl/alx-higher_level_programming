9b132f495077ace5b9dcf580c0b00ff42dfc9558
div-styl <gouhmada@gmail.com>
2023-07-11 12:38:16 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""define a class student"""
+
+
+class Student:
+    """represent the class student"""
+    def __init__(self, first_name, last_name, age):
+        """init values in the argumen func"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        func that retrieves a dictionary
+        representation of a Student
+        """
+        return self.__dict__.copy()
