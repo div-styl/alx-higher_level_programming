15f52719bd7641675269da8f967f5e211e95e7ea
div-styl <gouhmada@gmail.com>
2023-07-11 18:52:08 +0100

    task 12 and code style

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""define a func pascal_triangle.py"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of
+    integers representing the Pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+    trian = [[1]]
+    while len(trian) != n:
+        tri = trian[-1]
+        tmpo = [1]
+        for i in range(len(tri) - 1):
+            tmpo.append(tri[i] + tri[i + 1])
+        tmpo.append(1)
+        trian.append(tmpo)
+    return trian
