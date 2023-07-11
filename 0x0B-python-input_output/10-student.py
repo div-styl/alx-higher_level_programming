5c44952482cbae811c834dd73cb4d7d8fc630d59
div-styl <gouhmada@gmail.com>
2023-07-11 12:53:59 +0100

    update of the task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""define a class student"""
-
 
 class Student:
     """represent the class student"""
     def __init__(self, first_name, last_name, age):
-        """init values in the argumen func"""
+        """init values in the argument func"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
