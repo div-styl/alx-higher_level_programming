dda89d8001663c934092fc3c8cabddc1a08660f6
div-styl <gouhmada@gmail.com>
2023-07-11 11:23:54 +0100

    appeanding text

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""define func append_write"""
+
+
+def append_write(filename="", text=""):
+    """retunr the number of characters added"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
