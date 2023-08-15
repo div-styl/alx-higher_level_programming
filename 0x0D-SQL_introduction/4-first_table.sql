ff4763eb1958d8257d7aeced467d11638c07cabf
div-styl <gouhmada@gmail.com>
2023-08-15 20:51:33 +0100

    Update!

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
---- create table with values.
+-- create table if not exists already and fill it with values id and name
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
