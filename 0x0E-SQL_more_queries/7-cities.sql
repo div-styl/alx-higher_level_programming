87b4e2bb8a338c4f10135bef0bfc007b5f0ab275
div-styl <gouhmada@gmail.com>
2023-08-16 15:13:04 +0100

    Update!

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,7 @@
 -- create database if not exits 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use hbtn_0d_usa
+USE hbtn_0d_usa;
 -- CREATE TABLE CITIES
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
