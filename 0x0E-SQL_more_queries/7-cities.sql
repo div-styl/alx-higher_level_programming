cfa25e1571ac6c6c45bddd183fb7fe65126a6feb
div-styl <gouhmada@gmail.com>
2023-08-16 15:30:08 +0100

    Update!

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT  AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES state(id),
     name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES state(id)
 );
