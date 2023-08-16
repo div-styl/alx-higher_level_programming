16a3dbdc0fdedd974ba79f9b007c334c5dea67cf
div-styl <gouhmada@gmail.com>
2023-08-16 13:19:33 +0100

    update every user in it's own line

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- LIST THE PRIVILEGES OF USER IN MYSQL
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
