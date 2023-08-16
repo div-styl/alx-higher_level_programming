9de8178fb4e9e0da58708095f2d70571efc757c1
div-styl <gouhmada@gmail.com>
2023-08-16 21:47:23 +0100

    Update!

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,8 @@
 -- displays the number of shows linked to each.
 SELECT name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres_id
-LEFT JOIN tv_show ON tv_show_genres.show_id = tv_show.id
-WHERE tv_show.title = 'Dexter'
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 GROUP BY name
 ORDER BY name ASC;
