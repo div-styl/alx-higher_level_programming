cd07d65915236461843dbf9cfe83938545d5a588
div-styl <gouhmada@gmail.com>
2023-08-16 15:12:02 +0100

    create data base and table which refer to prev table state

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create database if not exits 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- CREATE TABLE CITIES
+CREATE TABLE IF NOT EXISTS cities(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES state(id),
+    name VARCHAR(256) NOT NULL
+);
