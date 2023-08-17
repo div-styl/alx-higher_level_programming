3504695e28aff2fb5bd1d7b469c3912d31c4f3e8
div-styl <gouhmada@gmail.com>
2023-08-17 18:51:09 +0100

    Update!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,13 +2,11 @@
 -- the database hbtn_0d_tvshows.
 SELECT title
 FROM tv_shows
-WHERE id NOT IN
-(
-SELECT title
+WHERE title NOT IN
+(SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-)
+WHERE tv_genres.name = 'Comedy')
 GROUP BY title
 ORDER BY title ASC;
