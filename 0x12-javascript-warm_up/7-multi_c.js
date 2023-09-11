c3de4048008c35c58f5ff423b955846c647ed260
div-styl <gouhmada@gmail.com>
2023-09-11 22:12:09 +0100

    all the tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined)
+{
+    console.log('Missing number of occurrences');
+} else {
+    const x = Number(process.argv[2]);
+    let counter = 0;
+    while (counter < x) {
+        console.log('C is fun');
+        counter++;
+    }
+}
