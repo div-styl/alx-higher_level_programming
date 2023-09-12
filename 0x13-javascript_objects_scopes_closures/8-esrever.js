8d8d5903dbee0ed434f63ffe26f9d561b874ed20
div-styl <gouhmada@gmail.com>
2023-09-12 13:06:21 +0100

    Update!

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const len = list.length - 1;
+  let cnt = 0;
+  while ((len - cnt) > 0) {
+    const temp = list[len - cnt];
+    list[len - cnt] = list[cnt];
+    list[cnt] = temp;
+    cnt++;
+  }
+  return list;
+};
