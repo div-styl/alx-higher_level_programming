52b9604e3da02f613b8bbefa39dd6c1497bb6a8d
div-styl <gouhmada@gmail.com>
2023-09-12 12:21:24 +0100

    define the w and h

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-// write class rectagle that define itself
+// write class rectangle that define itself
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
 }
 
 module.exports = Rectangle;
