021ef33e39af8f942066f8ab846d1d266f3b5d07
div-styl <gouhmada@gmail.com>
2023-09-16 12:02:42 +0100

    Update!

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Write a script that lists all City
+objects from the database hbtn_0e_101_usa
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
