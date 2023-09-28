e53ca909c77fe5ad4492967d72b2da81f854f692
div-styl <gouhmada@gmail.com>
2023-09-28 22:02:19 +0100

    use dash

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL and displays all HTTP methods the server will accept.
-curl -siLX "$1" | grep -i Allow | awk '{print $2}'
+curl -si -L -X "$1" | grep -i Allow | awk '{print $2}'
