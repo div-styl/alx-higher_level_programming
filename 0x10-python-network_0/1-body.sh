d9a6b6f77f0e014c6307dae2b92b062f106304c2
div-styl <gouhmada@gmail.com>
2023-09-28 20:50:15 +0100

    Update!

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passed as the first argument and displays the body of the response 
-curl -X DELETE "$1"
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s "$1"
