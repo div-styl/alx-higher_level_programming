3eac299c6c2977772bfee4f02e688f97fd9be385
div-styl <gouhmada@gmail.com>
2023-09-29 20:19:30 +0100

    post mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+from urllib import parse
+from sys import argv
+
+if __name__ == '__main__':
+    url = argv[1]
+    mail = {"email": argv[2]}
+    alxurl = parse.urlencode(mail).encode("ascii")
+
+    req = urllib.request.Request(url, alxurl)
+    with urllib.request.urlopen(req) as reques:
+        print(reques.read().encode("utf-8"))
+
