41dd3ac2f6a044d7b81a75253fbba96929e9e94f
div-styl <gouhmada@gmail.com>
2023-09-29 23:09:27 +0100

    Update!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+ists the 10 most recent
+commits on a given GitHub repository.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[1], argv[2]
+    )
+    r = requests.get(url)
+    cmmt = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                cmmt[i].get("sha"),
+                cmmt[i].get("commit").get("author").get("name")
+            ))
+    except IndexError:
+        pass
