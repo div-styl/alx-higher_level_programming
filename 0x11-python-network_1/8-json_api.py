000756a81c58c636d862ab2b86494a6ad0bb8c96
div-styl <gouhmada@gmail.com>
2023-09-29 22:40:56 +0100

    Update!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    let = "" if len(argv) == 1 else argv[1]
+    search = {"q": let}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=search)
+
+    try:
+        rep = r.json()
+        if rep == {}:
+            print(" No result")
+        else:
+            print("[{}] {}".format(rep.get("id"), rep.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
