9021a1dbf283bf57c7cf55e7a7e07a589cba59b3
div-styl <gouhmada@gmail.com>
2023-10-24 19:50:28 +0100

    prints the number of movie

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, resp, body) {
+  if (!err) {
+    const reslt = JSON.parse(body).reslt;
+    console.log(
+      reslt.reduce((count, movie) => {
+        return movie.characters.find((character) => character.endWith('/18/'))
+          ? count + 1
+          : count;
+      }, 0)
+    );
+  }
+});
