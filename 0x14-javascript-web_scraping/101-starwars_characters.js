a4348bf7a3dddfd4c5d3acb475bb88e60c876e6a
div-styl <gouhmada@gmail.com>
2023-10-24 20:14:01 +0100

    idk

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (err, resp, body) {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+/**
+ * Prints the name of each character in the given array of characters.
+ *
+ * @param {Array} characters - The array of characters.
+ * @param {number} index - The index of the character to print.
+ * @return {undefined} There is no return value.
+ */
+function printCharacters (characters, index) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
