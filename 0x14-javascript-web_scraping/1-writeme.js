6a3ce487e293e48e9b4ec23645993c6c1a536b16
div-styl <gouhmada@gmail.com>
2023-10-24 19:43:23 +0100

    Update!

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 // write a text in a file
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
-  console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) console.log(err);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
+const id = process.argv[2];
+
+request(API_URL + id, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else if (resp.statusCode === 200) {
+    const respjson = JSON.parse(body);
+    console.log(respjson.title);
+  } else {
+    console.log('Error code: ' + resp.statusCode);
+  }
+});
