bfa99bf8a8eca46b2f0ecaa7695e1af2169589f2
div-styl <gouhmada@gmail.com>
2023-10-24 20:09:39 +0100

    test

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
+const request = require("request");
+const API_URL = "https://swapi-api.hbtn.io/api/films/";
+const id = process.argv[2];
+
+request.get(API_URL + id, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  const p = data.characters;
+  for (const i in p) {
+    request.get(i, function (err, resp, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        const data1 = JSON.parse(body1);
+        console.log(data1);
+      }
+    });
+  }
+});
